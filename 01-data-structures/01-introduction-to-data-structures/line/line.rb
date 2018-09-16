aa68f0b4cb82afdcf6cc049cdc54facf2db013ea
Erin Leck <erinleck@gmail.com>
2018-09-15 21:04:27 -0600

    Update line.rb

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -1,6 +1,3 @@
-# A line of people at an amusement park ride
-# There is a front to the line, as well as a back.
-# People may leave the line whenever they see fit and those behind them take their place.
 class Line
   attr_accessor :members
 
@@ -9,26 +6,38 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+		members.delete(person)
   end
 
   def front
+		return members.first
   end
 
   def middle
+		middle = ((members.length)/2)
+		return members[middle]
   end
 
   def back
+		return members.last
   end
 
   def search(person)
+		if members.include?(person) === true
+			return person
+			else
+				return nil
+		end
   end
 
   private
 
   def index(person)
+		return members.index(person)
   end
 
 end
