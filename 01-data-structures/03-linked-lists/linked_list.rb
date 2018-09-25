c81d1f713dc718d2830542fda40248d6350266c1
Erin Leck <erinleck@gmail.com>
2018-09-25 02:43:37 +0000

    linked list & node class complete

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -4,27 +4,65 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
-  def add_to_tail(node)
-  end
-
-  # This method removes the last node in the lists and must keep the rest of the list intact.
+	def add_to_tail(node)
+		if @head.nil?
+			@head, @tail = node, node
+	  else
+	    current_node = @head
+	    while current_node.next
+	      current_node = current_node.next
+	    end
+	    current_node.next = node
+	    @tail = node
+		end
+	end
+  
   def remove_tail
+    if @head === @tail
+      @head, @tail = nil, nil
+    else
+      current_node = @head
+      while current_node.next && @tail != current_node.next
+        current_node = current_node.next
+      end
+      current_node.next = nil
+      @tail = current_node
+    end
   end
-
-  # This method prints out a representation of the list.
+  
   def print
+    current_node = @head
+		puts current_node.data
+    while current_node.next
+      current_node = current_node.next
+      puts "#{current_node.data}"
+    end
   end
 
-  # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    current_node = @head
+    while current_node.next
+      if @head == node
+        @head = @head.next
+      elsif current_node.next == node
+        current_node.next = node.next
+      end
+      current_node = current_node.next
+      if @tail == node
+        current_node.next = nil
+        @tail = current_node
+      end
+    end
   end
-
-  # This method adds `node` to the front of the list and must set the list's head to `node`.
+  
   def add_to_front(node)
+    node.next = @head
+    @head = node
   end
-
-  # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
+  
   def remove_front
+    temp = @head
+    @head = @head.next
+    return temp
   end
 end
