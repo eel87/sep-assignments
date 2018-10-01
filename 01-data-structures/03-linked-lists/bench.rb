03333ddeef0ec8d315658d67e0367e8e93c7a8c5
Erin Leck <erinleck@gmail.com>
2018-10-01 20:34:25 +0000

    update bench.rb and answers

--- 01-data-structures/03-linked-lists/bench.rb
+++ 01-data-structures/03-linked-lists/bench.rb
@@ -4,35 +4,32 @@ require_relative 'linked_list'
 
 #Compare the time it takes to create a 10,000 item Array to appending 10,000 items to a Linked List.
 
-array = Array.new(10000) {rand 1000}
-list = LinkedList.new
-
-10_000.times do
-  list.add_to_tail(Node.new(rand 1000))
-end
+a = []
 
 puts Benchmark.measure {
-  puts "array"
-  array
+  10000.times do |i|
+    a << i
+  end
 }
 
+l = LinkedList.new
+
 puts Benchmark.measure {
-  puts "list"
-  list
+  10000.times do |i|
+    n = Node.new(i)
+    l.add_to_tail(n)
+  end
 }
 
 #Compare the time it takes to access the 5000th element of the Array and the 5000th Node in the Linked List.
 
-findArrayElement = array.fetch(5000)
-
 puts Benchmark.measure {
-  puts "array"
-  findArrayElement
+  a.fetch(5000)
 }
 
+nextNode = l.head.next
+
 puts Benchmark.measure {
-  puts "list"
-  nextNode = list.head.next
   5_000.times do
     nextNode = nextNode.next
   end
@@ -41,18 +38,10 @@ puts Benchmark.measure {
 # Compare the time it takes to remove the 5000th element from the Array to removing the 5000th Node in the Linked List.
 # In the Array, the 5001st item becomes the 5000th, and so on.
 
-deleteArrayElement = array.delete_at(5000)
-
 puts Benchmark.measure {
-  puts "array"
-  deleteArrayElement
+  a.delete_at(5000)
 }
-
+  
 puts Benchmark.measure {
-  puts "list"
-  nextNode = list.head.next
-  5.times do
-    nextNode = nextNode.next
-  end
-  list.delete(nextNode)
+  l.delete(nextNode)
 }
