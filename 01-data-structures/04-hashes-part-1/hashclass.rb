bb20ae92607f11a9e8b910d08a389547135209d3
Erin Leck <erinleck@gmail.com>
2018-10-20 03:09:00 +0000

    hashclass 10/19

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,27 +1,47 @@
 class HashClass
+	attr_accessor :items
 
   def initialize(size)
     @items = Array.new(size)
+		@items.map! { |item| item = HashItem.new(@key, @value) }		
   end
 
-  def []=(key, value)
-  end
-
+ def []=(key, value)
+		index = index(key, size)
+		@items[index].key = key
+		@items[index].value = value
+		key = value
+  	# if @items[index].eql?(nil)
+  	#   @items[index] = @items[index[1]]
+		# elsif @items[index].eql?(@items[index[1]])
+		# 	return @items[index[1]]
+  	# else
+  	# 	resize 
+  	# 	# @items.each_with_index do |item, index|
+				
+		# 	# end
+		# end
+ end
 
   def [](key)
+		index =  index(key, @items.length)
+		return @items[index]
   end
 
-  def resize
-  end
-
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
-  def index(key, size)
-  end
 
   # Simple method to return the number of items in the hash
   def size
+		return @items.count
   end
 
+  def index(key, size)
+    index = key.sum % size
+  end
+  
+  def resize
+		s = @items.length
+		newItems = Array.new((s*2) - s)
+		newItems.map! { |item| item = HashItem.new(@key, @value) }
+		@items += newItems
+	end
 end
