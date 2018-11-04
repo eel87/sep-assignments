4a994ecf8ed6d8dd57d82a27d647f990c289697f
Erin Leck <erinleck@gmail.com>
2018-11-04 17:08:17 +0000

    open addressing 1

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -6,7 +6,25 @@ class OpenAddressing
   end
 
   def []=(key, value)
-  end
+    i = index(key, size)
+		if @items[i] == nil
+			@items[i] = Node.new(key, value)
+		# elsif @items[i].key == key
+		# 	@items[i].value = value
+		else
+			resize
+			i = index(key, size)
+			while @items[i].next
+				if @items[i].next == nil
+					@items[i] = Node.new(key, value)
+				# elsif @items[i].key == key
+				# 	@items[i].value = value
+				else
+				  resize
+				end
+			end
+		end
+	end
 
   def [](key)
     i = index(key, size)
@@ -22,6 +40,16 @@ class OpenAddressing
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    if @items[index] == nil
+      return index
+    end
+    while @items[index].next
+				if @items[i].next == nil
+				  return index
+				else
+				  return -1
+				end
+    end		
   end
 
   # Simple method to return the number of items in the hash
@@ -31,5 +59,15 @@ class OpenAddressing
 
   # Resize the hash
   def resize
+		size = (@items.length*2)
+		newItems = Array.new(size)
+		
+		@items.each_with_index do |item, index|
+			next if item == nil
+			newIndex = index(item.key, size)
+			newItems[newIndex] = item
+		end
+	  @items.replace(newItems)
   end
+  
 end
