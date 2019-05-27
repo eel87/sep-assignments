ba269ffc367194da411580c242ebbb9557eafe21
Erin Leck <erinleck@gmail.com>
2019-05-27 12:06:24 +0000

    quick_sort method redefined

--- 02-algorithms-and-complexity/algorithms-sorting/quick_sort.rb
+++ 02-algorithms-and-complexity/algorithms-sorting/quick_sort.rb
@@ -1,33 +1,23 @@
-def partition(collection, left_pointer, right_pointer, pivot)
-	print "collection = #{collection}\n"
-	while left_pointer <= right_pointer
-		while collection[left_pointer] < pivot
-			left_pointer += 1
-		end
-		while collection[right_pointer] > pivot
-			right_pointer -= 1
-		end
-		if left_pointer <= right_pointer
-			collection[left_pointer], collection[right_pointer] = collection[right_pointer], collection[left_pointer]
-			left_pointer += 1
-			right_pointer -= 1
-		end
-	end
-	return left_pointer
-end
+def quick_sort(array)
+  return array if array.length <= 1
+  pivot = array.delete_at(rand(array.length))
 
-def quicksort(collection, left = 0, right = collection.length - 1)
-	if left >= right
-		return
-	end
-	pivot = collection[(left + right)/2].floor
-	index = partition(collection, left, right, pivot)
-	quicksort(collection, left, index - 1)
-	quicksort(collection, index, right)
-	return collection
+  left = Array.new
+  right = Array.new
+
+  array.each do |x|
+    if x <= pivot
+      left << x
+    else
+      right << x
+    end
+  end
+
+  return *quick_sort(left), pivot , *quick_sort(right)
+
 end
 
 
 collection = Array.new(15) { rand(0..300) }
 
-print quicksort(collection)
+print quick_sort(collection)
