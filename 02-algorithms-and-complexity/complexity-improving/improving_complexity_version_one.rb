7d315caf9469b988a84d61c3991a6827a8f50f1a
Erin Leck <erinleck@gmail.com>
2019-07-03 01:23:25 +0000

    version one corrected

--- 02-algorithms-and-complexity/complexity-improving/improving_complexity_version_one.rb
+++ 02-algorithms-and-complexity/complexity-improving/improving_complexity_version_one.rb
@@ -8,13 +8,15 @@ def poorly_written_ruby(*arrays)
       if val <= sorted_array[i]
         sorted_array.insert(i, val)
         break
-      elsif i == sorted_array.length - 1
-        sorted_array << val
-        break
       end
       i+=1
     end
+    
+    if i == sorted_array.length
+      sorted_array << val
+    end
   end
+  
   sorted_array
 end
 
@@ -23,5 +25,3 @@ array1 = Array.new(3) { rand(0..300) }
 array2 = Array.new(3) { rand(0..300) }
 array3 = Array.new(3) { rand(0..300) }
 print poorly_written_ruby(array1, array2, array3)
-
-# Your code for the version-one does not sort correctly anymore. As written now, the inner loop will only ever run once. A hint I can give you is that the conditional I you removed is equivalent to the while loop condition.
